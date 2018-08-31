b62c26754122cf5c3957b1b651e3302eab217557
Saul van der Walt <saulvdw@gmail.com>
2018-08-31 18:47:18 +0200

    make sure it's the right one

--- assets/js/next.js
+++ assets/js/next.js
@@ -10,13 +10,14 @@ const cacheScript = (
   if (cached != null && !fresh) return fn(cached)
   fetch(url).then(r => r.text()).then(src => localStorage.setItem(url, fn(src) || src))
 }
-
-cacheScript(
-  location.host.includes('localhost') ? 'http://localhost:2018/dist/rilti.js' : 'https://rawgit.com/SaulDoesCode/rilti.js/experimental/dist/rilti.min.js',
+const isDevBox = location.host.includes('localhost')
+cacheScript(isDevBox
+  ? 'http://localhost:2018/dist/rilti.js'
+  : 'https://rawgit.com/SaulDoesCode/rilti.js/experimental/dist/rilti.min.js',
   src => {
     const script = document.createElement('script')
     script.textContent = src + ';\n'
-    cacheScript('/assets/js/next-view.js', src => {
+    cacheScript((isDevBox ? '' : '/assets') + '/js/next-view.js', src => {
       script.textContent += `\n;rilti.run(() => {\n${src}\n});\n`
       document.head.appendChild(script)
       localStorage.setItem('fresh', false)
