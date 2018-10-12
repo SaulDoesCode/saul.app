0cc8e977dad975ab204077cde6a50c0024590e92
Saul van der Walt <saulvdw@gmail.com>
2018-10-12 18:16:29 +0200

    why are modules so hard

--- backend/server.go
+++ backend/server.go
@@ -85,8 +85,13 @@ func Init(configfile string) {
 		http.Redirect(res, req, target, http.StatusTemporaryRedirect)
 	}))
 
+	addrs := []string{}
+	for _, val := range Config.Get("db_address").Array() {
+		addrs = append(addrs, val.String())
+	}
+
 	setupDB(
-		[]string{"http://localhost:8529"},
+		addrs,
 		Config.Get("db_name").String(),
 		Config.Get("db_username").String(),
 		Config.Get("db_password").String(),
--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
-module saulapp
-
-require github.com/sauldoescode/saul.app/backend v0.0.0-20181012153605-cf6716536246 // indirect
+module github.com/sauldoescode/saul.app
