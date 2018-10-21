e94957d17faef72a64f5e6c1000618346acc41ff
Saul van der Walt <saulvdw@gmail.com>
2018-10-21 12:20:36 +0200

    memfiles are overkill, and slightly broken actually.

--- backend/admin.go
+++ backend/admin.go
@@ -2,6 +2,6 @@ package backend
 
 func initAdmin() {
 	Server.GET("/admin", AdminHandle(func(c ctx, user *User) error {
-		return MFI.ServeFile(c, "/admin.html")
+		return c.File(AssetsFolder + "/admin.html")
 	}))
 }
